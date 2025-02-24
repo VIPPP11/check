e4275cb9195af35c1396b08d0dbe3db4dff9f4b5
VIPPP11 <ko114koko@gmail.com>
2025-02-24 17:42:58 +0700

    Update player.js

--- player.js
+++ player.js
@@ -99,7 +99,7 @@ function initializePlayer(client) {
                 iconURL: musicIcons.playerIcon,
                 url: config.SupportServer
             })
-            .setFooter({ text: `Developed by SSRR | Prime Music v1.2`, iconURL: musicIcons.heartIcon })
+            .setFooter({ text: `Developed by Penhfgold_ | Bình Gold`, iconURL: musicIcons.heartIcon })
             .setTimestamp()
             .setDescription(  
                 `- **Title:** [${track.info.title}](${track.info.uri})\n` +
