e046d274dd611acf77febe0055ab73a94897a221
VIPPP11 <ko114koko@gmail.com>
2025-02-24 17:33:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   TOKEN: "",
   language: "vi",
   ownerID: ["657589847510482944", ""], 
-  mongodbUri : "",
+  mongodbUri : "mongodb+srv://shiva@musicbotyt.ouljywv.mongodb.net/?retryWrites=true&w=majority",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
