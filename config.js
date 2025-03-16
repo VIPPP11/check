092198c34637ba942b9371164df0d1ba722470d9
VIPPP11 <ko114koko@gmail.com>
2025-03-16 20:19:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -16,12 +16,12 @@ module.exports = {
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
-     {
-      name: "XX",
-      password: "NAIGLAVA-dash.techbyte.host",
-      host: "lavahatry4.techbyte.host",
-      port:  3000,
+{
+      name: "Avinan",
+      password: "avinan",
+      host: "new-york-node-1.vortexcloud.xyz",
+      port:  9323,
       secure: false
-    }
+} 
   ]
 }
